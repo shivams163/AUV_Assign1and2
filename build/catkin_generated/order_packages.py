37747338150400cf7e5efee439dce112578a8455
Shivam Singhal <shivamsinghal16032003@gmail.com>
2021-07-22 02:12:31 +0530

    Assignment 1 and 2

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/shivams/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/shivams/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/shivams/catkin_ws/devel;/opt/ros/noetic' != '' else []
